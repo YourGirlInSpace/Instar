8652ab6da907202d1c872ed4cd598f91aae7b4e3
YourGirlInSpace <mypaxandromeda@gmail.com>
2023-07-01 10:48:48 -0700

    Starting Instar in the background, removing TRACE flag for Release builds

--- Commands/ApplicationStart/StartInstar.sh
+++ Commands/ApplicationStart/StartInstar.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/Instar/bin/InstarBot --config-path /Instar/bin/Config/Instar.conf.json >> /Instar/Instar.log
+/Instar/bin/InstarBot --config-path /Instar/bin/Config/Instar.conf.json &
