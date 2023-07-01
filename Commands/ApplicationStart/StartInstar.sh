54f3e52e8155762e3501251eeb507bd9aae075cf
YourGirlInSpace <mypaxandromeda@gmail.com>
2023-07-01 10:59:59 -0700

    CodeDeploy doesn't like long running scripts.

--- Commands/ApplicationStart/StartInstar.sh
+++ Commands/ApplicationStart/StartInstar.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/Instar/bin/InstarBot --config-path /Instar/bin/Config/Instar.conf.json &
+/Instar/bin/InstarBot --config-path /Instar/bin/Config/Instar.conf.json > /dev/null 2> /dev/null < /dev/null &
