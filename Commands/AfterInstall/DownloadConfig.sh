d6ead89d2e9347cee193493d9db32c1273dbb8e7
YourGirlInSpace <mypaxandromeda@gmail.com>
2023-07-01 17:34:55 -0700

    Updated AfterInstall, ApplicationStart and ValidateService commands

--- Commands/AfterInstall/DownloadConfig.sh
+++ Commands/AfterInstall/DownloadConfig.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-INSTAR_CONF_FILE=Instar.conf.json
+INSTAR_CONF_FILE=$(../Global/GetInstarConfiguration.sh)
+
+if [ $? -ne 0 ]; then
+  echo "Could not get config file."
+  exit 1
+fi 
+
 aws s3api get-object --bucket instarconfig --region us-west-2 --key $INSTAR_CONF_FILE /Instar/bin/Config/$INSTAR_CONF_FILE
