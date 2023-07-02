88a0b29dc09a8be8328ef6887133107cdb45eaf4
YourGirlInSpace <mypaxandromeda@gmail.com>
2023-07-01 17:34:55 -0700

    Updated AfterInstall, ApplicationStart and ValidateService commands

--- Commands/ValidateService/CheckInstarRunning.sh
+++ Commands/ValidateService/CheckInstarRunning.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Wait for 30 seconds before checking.
+# Sometimes Instar is running while it tries to
+# compile error reports.
+sleep 30
+
 ps -C InstarBot
 if [ $? -ne 0 ]; then
   echo "InstarBot did not start successfully."
