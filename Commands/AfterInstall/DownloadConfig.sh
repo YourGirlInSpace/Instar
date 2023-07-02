d652b79b292f547695cee219106a6a245d84625f
YourGirlInSpace <mypaxandromeda@gmail.com>
2023-07-01 17:51:30 -0700

    Deployment script update

--- Commands/AfterInstall/DownloadConfig.sh
+++ Commands/AfterInstall/DownloadConfig.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-INSTAR_CONF_FILE=$(../Global/GetInstarConfiguration.sh)
-
-if [ $? -ne 0 ]; then
+if ! INSTAR_CONF_FILE=$("$SCRIPT_DIR"/../Global/GetInstarConfiguration.sh); then
   echo "Could not get config file."
   exit 1
 fi 
 
-aws s3api get-object --bucket instarconfig --region us-west-2 --key $INSTAR_CONF_FILE /Instar/bin/Config/$INSTAR_CONF_FILE
+aws s3api get-object --bucket instarconfig --region us-west-2 --key "$INSTAR_CONF_FILE" "/Instar/bin/Config/$INSTAR_CONF_FILE"
--- Commands/ApplicationStart/StartInstar.sh
+++ Commands/ApplicationStart/StartInstar.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 startInstar() {
-  local INSTAR_CONF_FILE=$(../Global/GetInstarConfiguration.sh)
-  
-  if [ $? -ne 0 ]; then
+  local INSTAR_CONF_FILE;
+
+  if ! INSTAR_CONF_FILE=$("$SCRIPT_DIR"/../Global/GetInstarConfiguration.sh); then
     echo "Could not get config file."
     exit 1
   fi 
   
   local INSTAR_CONF="/Instar/bin/Config/$INSTAR_CONF_FILE"
 
-  /Instar/bin/InstarBot --config-path $INSTAR_CONF > /dev/null 2> /dev/null < /dev/null &
+  /Instar/bin/InstarBot --config-path "$INSTAR_CONF" > /dev/null 2> /dev/null < /dev/null &
 }
 
 startInstar
